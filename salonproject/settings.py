d216e49deaf1c806afa3ab7dacdb4b05357eb4cf
mollybeach <mollyjbeach@gmail.com>
2021-07-19 21:50:21 -0700

    static

--- salonproject/settings.py
+++ salonproject/settings.py
@@ -204,7 +204,7 @@ LOGGING = {
     'loggers': {
         'django': {
             'handlers': ['console'],
-             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
         },
     },
 }
