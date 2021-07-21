3edba348d6a556f3579492285929d3e9ff29978c
mollybeach <mollyjbeach@gmail.com>
2021-07-21 15:23:55 -0700

    change email format

--- salonapp/views.py
+++ salonapp/views.py
@@ -117,13 +117,16 @@ def calendar(request):
             saveobj.email=request.POST.get('email')
             saveobj.telephone=request.POST.get('telephone')
             saveobj.appointment_date=request.POST.get('appointment_date')
-            subject='Appointment with MadeleineSalonDeCoiffure'
-            message='Thank you for scheduling an appointment with Madeleine for this date'
+            subjectforclient='Appointment with MadeleineSalonDeCoiffure'
+            subjectforhairdresser='Appointment with {saveobj.firstname} {saveobj.lastname} on {saveobj.appointmentdate} on  {saveobj.appointmentdate}'
+            messageforclient='Thank you for scheduling an appointment with Madeleine for this date {saveobj.appointmentdate}'
+            messageforhairdresser='You are scheduled to for an appointment with {saveobj.firstname} {saveobj.lastname} on {saveobj.appointmentdate} make sure to follow up with them at {saveobj.telephone}'
             cursor=connection.cursor()
             cursor.execute("INSERT INTO schedule(first_name, last_name, email, telephone, appointment_date) values(' "+saveobj.first_name+ "', ' "+saveobj.last_name+ "',  ' "+saveobj.email+ "',  ' "+saveobj.telephone+ "',  '" + saveobj.appointment_date+ "' )")
-            messages.success(request, "Client name  "+saveobj.first_name+ " "+saveobj.last_name+ "has successfully scheduled appointment on "+ saveobj.appointment_date)
+            messages.success(request, "Thank you! "+saveobj.first_name+ " "+saveobj.last_name+ "has successfully scheduled appointment on "+ saveobj.appointment_date)
             try:
-                send_mail(subject, message, saveobj.email, ['madeleinesalondecoiffure@gmail.com'])
+                send_mail(subjectforclient, messageforclient, saveobj.email, saveobj.email)
+                send_mail(subjectforhairdresser, messageforhairdresser, saveobj.email, ['madeleinesalondecoiffure@gmail.com'])
             except BadHeaderError:
                 return HttpResponse('Invalid header found.')
             return render(request, 'calendar.html')
@@ -132,7 +135,7 @@ def calendar(request):
 
 
 
-
+#['madeleinesalondecoiffure@gmail.com']
 
 
 
