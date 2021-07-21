1b9b5c1304d17d48cf2fd9229e3d20157d9e85f2
mollybeach <mollyjbeach@gmail.com>
2021-07-20 21:31:42 -0700

    fixed heroku database connection

--- salonapp/views.py
+++ salonapp/views.py
@@ -124,7 +124,7 @@ def calendar(request):
 #make a calendar feature that we can put in our app
 #calendar user can add events to a calender and see all events in a calender
 #calendar user can delete events from a caldenar
-#calendar can see all the events in a calendarr
+#calendar can see all the events in a calendarrr
 #schedule a meetingf
 #schedule appointment
 
