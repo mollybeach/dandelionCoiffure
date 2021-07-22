6977ae0ad8ddf800a59d237b1bee06a5cb16fd6d
mollybeach <mollyjbeach@gmail.com>
2021-07-21 17:10:07 -0700

    cal

--- app.py
+++ app.py
@@ -1,16 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 from datetime import datetime
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/booklist')
 def homepage():
-    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
+    #books=db.execute("SELECT * FROM booklist order by bookid")
+    books='books'
+    return render_template("BookList.html", books=books)
 
-    return """
-    <h1>Hello heroku</h1>
-    <p>It is currently {time}.</p>
-    <img src="http://loremflickr.com/600/400" />
-    """.format(time=the_time)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
