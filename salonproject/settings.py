fa843fe091732ed1f9b3731888e38b0b463b0e07
mollybeach <mollyjbeach@gmail.com>
2021-07-21 23:25:38 -0700

    test changing static location

--- salonproject/settings.py
+++ salonproject/settings.py
@@ -115,23 +115,32 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
-
+'''
 # STATIC FILES (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = "/static/"
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static_collections'),
-)
+#STATICFILES_DIRS = (
+ #   os.path.join(BASE_DIR, 'static_collections'),
+#)
+'''
+# settings.py
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+STATICFILES_DIRS = (os.path.join(
+    BASE_DIR, "salonapp", "static"),)
+STATIC_ROOT = os.path.join(
+    os.path.dirname(BASE_DIR), "deployment", "collected_static")
+MEDIA_ROOT = os.path.join(
+    os.path.dirname(BASE_DIR), "deployment", "media")
 
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 )
 
-MEDIA_URL = '/media/'
+MEDIA_URL = 'deployment/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 
